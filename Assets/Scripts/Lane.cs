1314fc91272584dc7a689576674e32b3c89e91a1
Shikatastrophe <shikatastrophe@gmail.com>
2023-05-30 23:36:04 -0600

    Agregue un script para el final, pero no funciona

---
+++ Assets/Scripts/Lane.cs
@@ -0,0 +1,81 @@
+﻿using Melanchall.DryWetMidi.Interaction;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lane : MonoBehaviour
+{
+    public Melanchall.DryWetMidi.MusicTheory.NoteName noteRestriction;
+    public KeyCode input; //Se debe de cambiar a el input de arduino
+    public KeyCode input2; //Se debe de cambiar a el input de arduino
+    public int input3; //Se debe de cambiar a el input de arduino
+    public GameObject notePrefab;
+    List<Note> notes = new List<Note>();
+    public List<double> timeStamps = new List<double>();
+
+    int spawnIndex = 0;
+    int inputIndex = 0;
+
+    public void SetTimeStamps(Melanchall.DryWetMidi.Interaction.Note[] array)
+    {
+        foreach (var note in array)
+        {
+            if (note.NoteName == noteRestriction)
+            {
+                var metricTimeSpan = TimeConverter.ConvertTo<MetricTimeSpan>(note.Time, SongManager.midiFile.GetTempoMap());
+                timeStamps.Add((double)metricTimeSpan.Minutes * 60f + metricTimeSpan.Seconds + (double)metricTimeSpan.Milliseconds / 1000f);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnIndex < timeStamps.Count)
+        {
+            if (SongManager.GetAudioSourceTime() >= timeStamps[spawnIndex] - SongManager.Instance.noteTime)
+            {
+                var note = Instantiate(notePrefab, transform);
+                notes.Add(note.GetComponent<Note>());
+                note.GetComponent<Note>().assignedTime = (float)timeStamps[spawnIndex];
+                spawnIndex++;
+            }
+        }
+
+        if (inputIndex < timeStamps.Count)
+        {
+            double timeStamp = timeStamps[inputIndex];
+            double marginOfError = SongManager.Instance.marginOfError;
+            double audioTime = SongManager.GetAudioSourceTime() - (SongManager.Instance.inputDelayInMilliseconds / 1000.0);
+
+            if (Input.GetKeyDown(input) || Input.GetKeyDown(input2)) //debe de ser alterado para funcionar con el juego 
+            {
+                if (Math.Abs(audioTime - timeStamp) < marginOfError)
+                {
+                    Hit();
+                    print($"Hit on {inputIndex} note");
+                    Destroy(notes[inputIndex].gameObject);
+                    inputIndex++;
+                }
+                else
+                {
+                    print($"Hit inaccurate on {inputIndex} note with {Math.Abs(audioTime - timeStamp)} delay");
+                }
+            }
+            if (timeStamp + marginOfError <= audioTime)
+            {
+                Miss();
+                print($"Missed {inputIndex} note");
+                inputIndex++;
+            }
+        }       
+    
+    }
+    private void Hit()
+    {
+        ScoreManager.Hit();
+    }
+    private void Miss()
+    {
+        ScoreManager.Miss();
+    }
+}
---
+++ Assets/Scripts/ScoreResults.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreResults : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            animator.SetBool("isPlaying", true);
+        }
+    }
+}
